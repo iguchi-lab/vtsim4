d22be270b60ded35063b88567c2fd6822aba96ba
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-02-03 10:07:48 +0900

    Create fan_sts.py

---
+++ vtsim/fan_sts.py
@@ -0,0 +1,14 @@
+#YCC Fan Spec
+qmax_YCC_L, pmax_YCC_L, q1_YCC_L, p1_YCC_L = 100 / 3600, 110.0, 100 / 3600,  95.0
+qmax_YCC_M, pmax_YCC_M, q1_YCC_M, p1_YCC_M = 200 / 3600, 110.0, 200 / 3600,  80.0
+qmax_YCC_H, pmax_YCC_H, q1_YCC_H, p1_YCC_H = 390 / 3600, 110.0, 200 / 3600,  80.0
+
+#RARA Fan Spec
+qmax_RARA0, pmax_RARA0, q1_RARA0, p1_RARA0 = 100 / 3600,  10.0,   0 / 3600,  10.0
+qmax_RARA1, pmax_RARA1, q1_RARA1, p1_RARA1 = 150 / 3600,  80.0,   0 / 3600,  80.0
+qmax_RARA2, pmax_RARA2, q1_RARA2, p1_RARA2 = 210 / 3600, 120.0,   0 / 3600, 120.0
+
+#SUMIKA Fan Sped
+q_SUMIKA = 200 / 3600
+
+##
