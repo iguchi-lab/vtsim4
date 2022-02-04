daed9fe2389756465c5e1f8b214f8008fef363de
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-02-04 13:29:26 +0900

    Update fan.py

--- vtsim/fan.py
+++ vtsim/fan.py
@@ -1,11 +1,3 @@
-YCC_L:  int = 1
-YCC_M:  int = 2
-YCC_H:  int = 3
-RARA0:  int = 4
-RARA1:  int = 5
-RARA2:  int = 6
-SUMIKA: int = 7
-
 class fan_spec:
     def __init__(self):
         self.qmax = {}
@@ -13,11 +5,11 @@ class fan_spec:
         self.q1   = {}
         self.p1   = {}
 
-    def set_spec(self, num, qmax, pmax, q1, p1):
-        self.qmax[num] = qmax
-        self.pmax[num] = pmax
-        self.q1[num]   = q1
-        self.p1[num]   = p1
+    def set_spec(self, name, qmax, pmax, q1, p1):
+        self.qmax[name] = qmax
+        self.pmax[name] = pmax
+        self.q1[name]   = q1
+        self.p1[name]   = p1
 
     def fan_dict(self, df, dic):
         qmax_d, pmax_d, q1_d, p1_d = {}, {}, {}, {}
@@ -37,17 +29,17 @@ class fan_spec:
 
 fs  = fan_spec()
 
-fs.set_spec(YCC_L,  100 / 3600, 110.0, 100 / 3600,  95.0)
-fs.set_spec(YCC_M,  200 / 3600, 110.0, 200 / 3600,  80.0)
-fs.set_spec(YCC_H,  390 / 3600, 110.0, 200 / 3600,  80.0)
+fs.set_spec('YCC_L',  100 / 3600, 110.0, 100 / 3600,  95.0)
+fs.set_spec('YCC_M',  200 / 3600, 110.0, 200 / 3600,  80.0)
+fs.set_spec('YCC_H',  390 / 3600, 110.0, 200 / 3600,  80.0)
 
-fs.set_spec(RARA0,  100 / 3600,  10.0,   0 / 3600, 10.0)
-fs.set_spec(RARA1,  150 / 3600,  80.0,   0 / 3600, 80.0)
-fs.set_spec(RARA2,  210 / 3600, 120.0,   0 / 3600, 120.0)
+fs.set_spec('RARA0',  100 / 3600,  10.0,   0 / 3600, 10.0)
+fs.set_spec('RARA1',  150 / 3600,  80.0,   0 / 3600, 80.0)
+fs.set_spec('RARA2',  210 / 3600, 120.0,   0 / 3600, 120.0)
 
-fs.set_spec(SUMIKA, 200 / 3600, 100.0, 200 / 3600, 100.0)
+fs.set_spec('SUMIKA', 200 / 3600, 100.0, 200 / 3600, 100.0)
 
-
+#旧バージョン
 #YCC Fan Spec
 qmax_YCC_L, pmax_YCC_L, q1_YCC_L, p1_YCC_L = 100 / 3600, 110.0, 100 / 3600,  95.0
 qmax_YCC_M, pmax_YCC_M, q1_YCC_M, p1_YCC_M = 200 / 3600, 110.0, 200 / 3600,  80.0
