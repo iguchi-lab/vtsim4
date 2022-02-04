68cf5d03e7078ceab32c9be3b4674d20fe206035
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-02-04 12:03:30 +0900

    Update fan.py

--- vtsim/fan.py
+++ vtsim/fan.py
@@ -1,13 +1,35 @@
-#YCC Fan Spec
-qmax_YCC_L, pmax_YCC_L, q1_YCC_L, p1_YCC_L = 100 / 3600, 110.0, 100 / 3600,  95.0
-qmax_YCC_M, pmax_YCC_M, q1_YCC_M, p1_YCC_M = 200 / 3600, 110.0, 200 / 3600,  80.0
-qmax_YCC_H, pmax_YCC_H, q1_YCC_H, p1_YCC_H = 390 / 3600, 110.0, 200 / 3600,  80.0
+YCC_L:  int = 1
+YCC_M:  int = 2
+YCC_H:  int = 3
+RARA0:  int = 4
+RARA1:  int = 5
+RARA2:  int = 6
+SUMIKA: int = 7
 
-#RARA Fan Spec
-qmax_RARA0, pmax_RARA0, q1_RARA0, p1_RARA0 = 100 / 3600,  10.0,   0 / 3600,  10.0
-qmax_RARA1, pmax_RARA1, q1_RARA1, p1_RARA1 = 150 / 3600,  80.0,   0 / 3600,  80.0
-qmax_RARA2, pmax_RARA2, q1_RARA2, p1_RARA2 = 210 / 3600, 120.0,   0 / 3600, 120.0
+class fan_spec:
+    def __init__(self):
+        self.qmax = {}
+        self.pmax = {}
+        self.q1   = {}
+        self.p1   = {}
 
-#SUMIKA Fan Sped
-q_SUMIKA = 200 / 3600
+    def set_spec(self, num, qmax, pmax, q1, p1):
+        self.qmax[num] = qmax
+        self.pmax[num] = pmax
+        self.q1[num]   = q1
+        self.p1[num]   = p1
 
+    def fan_dict(self, df, dic):
+        qmax_d, pmax_d, q1_d, p1_d = {}, {}, {}, {}
+        for k in dic:  
+            qmax_d[k] = self.qmax[dic[k]]
+            pmax_d[k] = self.pmax[dic[k]]
+            q1_d[k]   = self.q1[dic[k]]
+            p1_d[k]   = self.p1[dic[k]]
+
+        qmax = df.replace(qmax_d).to_list()
+        pmax = df.replace(pmax_d).to_list()
+        q1   = df.replace(q1_d).to_list()
+        p1   = df.replace(p1_d).to_list()
+        
+        return({'qmax': qmax, 'pmax': pmax, 'q1': q1, 'p1': p1})
