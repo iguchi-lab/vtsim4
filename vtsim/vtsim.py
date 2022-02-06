6cb78d1efecfd0bde93c6433f50b8db40d23d598
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-02-06 09:24:34 +0900

    Update vtsim.py

--- vtsim/vtsim.py
+++ vtsim/vtsim.py
@@ -66,6 +66,10 @@ def encode(object):
     if isinstance(object, pd.core.indexes.datetimes.DatetimeIndex): return(object.strftime('%Y/%m/%d %H:%M:%S').to_list())
     if isinstance(object, pd.core.series.Series):                   return(object.to_list())
 
+def to_json(input):
+    with open('tutorial_ibec.json', 'w') as f:
+        json.dump(input, f, default = vt.encode, ensure_ascii = False, indent = 4)
+
 def json(input):
     return(json.dumps(input, default = vt.encode, ensure_ascii = False, indent = 4))
 
