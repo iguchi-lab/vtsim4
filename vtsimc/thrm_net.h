ed894cdeec9ebee1d496392ebd95d84b8c60afbf
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-02-12 19:27:44 +0900

    Update thrm_net.h

--- vtsimc/thrm_net.h
+++ vtsimc/thrm_net.h
@@ -6,7 +6,7 @@ public:
     
     vector<double>      cdtc;                                                               //コンダクタンス
     vector<double>      ms;                                                                 //日射取得率
-    vector<double>      area, rg;                                                           //面積、表面熱抵抗
+    vector<double>      area;                                                               //面積
     double              phi_0;                                                              //応答係数                                                        
     vector<double>      cof_r, cof_phi, t_dash_gs;
     vector<double>      qt;                                                                 //熱流
@@ -30,7 +30,7 @@ public:
                 return cdtc[ts] * dt;                                                       //単純熱回路網の場合
             case TN_GROUND:                                                                 //地盤の場合  
                 for(int i = 0; i < 10; i++)     sum_t_dash_gs += t_dash_gs[i];              //合計の計算
-                return area[ts] / (rg[ts] + phi_0) * (dt - sum_t_dash_gs);                  //熱流の計算
+                return area[ts] / phi_0 * (dt - sum_t_dash_gs);                             //熱流の計算
             default:
                 return 0.0;
         }
@@ -40,10 +40,9 @@ public:
     double get_qt_h(double h_inp, long ts) {return h_inp;}                                  //発熱
     
     void refresh(double t_uf_t_1, double t_g_t_1, long ts){
-        double sum_t_dash_gs = 0.0;                                                                 //項別成分の合計
-        for(int i = 0; i < 10; i++)         sum_t_dash_gs += t_dash_gs[i];                          //合計の計算
-        double t_gs = (phi_0 * t_uf_t_1 + rg[ts] * (sum_t_dash_gs + t_g_t_1)) / (rg[ts] + phi_0);   //表面温度の計算
+        double sum_t_dash_gs = 0.0;                                                         //項別成分の合計
+        for(int i = 0; i < 10; i++)         sum_t_dash_gs += t_dash_gs[i];                  //合計の計算
         for(int i = 0; i < 10; i++)
-            t_dash_gs[i] = cof_phi[i] * (t_uf_t_1 - t_gs) / rg[ts] + cof_r[i] * t_dash_gs[i];       //吸熱応答の項別成分の計算
+            t_dash_gs[i] = cof_phi[i] * qt[ts] / area[ts] + cof_r[i] * t_dash_gs[i];        //吸熱応答の項別成分の計算
     }
 };
